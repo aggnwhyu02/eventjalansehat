19866ebe6db5d0c03612f55a9549c14765cd66b2
aggnwhyu02 <agungwahyuutama52@gmail.com>
2026-05-14 18:34:04 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: ['reasonable-elegance-production-4632.up.railway.app']
+  }
 })
